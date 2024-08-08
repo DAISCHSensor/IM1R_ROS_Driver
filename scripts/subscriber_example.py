7c21db092e6a211878d4d26580331e0e55230eeb
xiaorong.wang <49577496+alien-wxr@users.noreply.github.com>
2024-08-08 05:44:30 -0700

    [FEAT]: add node.py and an example.py

---
+++ scripts/subscriber_example.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+import inspect
+from sensor_msgs.msg import Imu, Temperature
+from im1r_ros_driver.msg import IM1R_EXTRA
+
+def callback(data):
+    attributes = inspect.getmembers(data, lambda a: not (inspect.isroutine(a)))
+    rospy.loginfo(data._type)
+    for a in attributes:
+        if not (a[0].startswith('_') or a[0].endswith('_')):
+            rospy.loginfo("%s: %s" % (a[0], a[1]))
+
+def main():
+    rospy.init_node('im1r_data_subscriber', anonymous=True)
+
+    rospy.Subscriber('imu/data', Imu, callback)
+    rospy.Subscriber('temperature', Temperature, callback)
+    rospy.Subscriber('im1r/extra', IM1R_EXTRA, callback)
+
+    rospy.loginfo("IM1R Data Subscriber Node Started. Waiting for data...")
+    rospy.spin()
+
+if __name__ == "__main__":
+    main()
