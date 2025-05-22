38508570d4544b21a6401892f9f1dc995efca9c5
changblooog <bolong.chang@daisch.com>
2025-05-21 20:35:06 -0700

    [FEAT]: Add multi-protocol support

--- scripts/daisch_im1r_node.py
+++ scripts/daisch_im1r_node.py
@@ -10,7 +10,7 @@ import math
 
 # Constants
 TEMP_DBL = -1.0
-USED_FRAME_LEN = 68
+MIN_FRAME_LEN = 68
 FRAME_ID = "IM1R"
 DEFAULT_PORT = '/dev/ttyUSB0'
 DEFAULT_BAUDRATE = 115200
@@ -67,14 +67,21 @@ def publish_imu_data(pub, stamp, data):
     msg.angular_velocity.x = data['GyroX'] * (math.pi / 180)
     msg.angular_velocity.y = data['GyroY'] * (math.pi / 180)
     msg.angular_velocity.z = data['GyroZ'] * (math.pi / 180)
-    quaternion = euler_to_quaternion(data['Roll'], data['Pitch'], data['Yaw'])
-    msg.orientation.w = quaternion[0]
-    msg.orientation.x = quaternion[1]
-    msg.orientation.y = quaternion[2]
-    msg.orientation.z = quaternion[3]
+
+    if all(k in data and data[k] is not None for k in ['Quat0', 'Quat1', 'Quat2', 'Quat3']):
+            msg.orientation.w = data['Quat0']
+            msg.orientation.x = data['Quat1']
+            msg.orientation.y = data['Quat2']
+            msg.orientation.z = data['Quat3']
+    else:
+        quaternion = euler_to_quaternion(data['Roll'], data['Pitch'], data['Yaw'])
+        msg.orientation.w = quaternion[0]
+        msg.orientation.x = quaternion[1]
+        msg.orientation.y = quaternion[2]
+        msg.orientation.z = quaternion[3]
+
     # msg.orientation_covariance[0] = msg.orientation_covariance[4] = msg.orientation_covariance[8] = TEMP_DBL
     pub.publish(msg)
-    # rospy.loginfo(msg._type)
 
 def publish_temperature(pub, stamp, data):
     msg = Temperature()
@@ -87,20 +94,24 @@ def publish_temperature(pub, stamp, data):
 
 def publish_extra_data(pub, data):
     msg = IM1R_EXTRA()
-    msg.count = data['Count']
-    msg.timestamp = data['Timestamp']
-    msg.pitch = data['Pitch']
-    msg.roll = data['Roll']
-    msg.yaw = data['Yaw']
-    msg.imu_status = data['IMUStatus']
-    msg.gyro_bias_x = data['GyroBiasX'] * (math.pi / 180)
-    msg.gyro_bias_y = data['GyroBiasY'] * (math.pi / 180)
-    msg.gyro_bias_z = data['GyroBiasZ'] * (math.pi / 180)
-    msg.gyro_static_bias_x = data['GyroStaticBiasX'] * (math.pi / 180)
-    msg.gyro_static_bias_y = data['GyroStaticBiasY'] * (math.pi / 180)
-    msg.gyro_static_bias_z = data['GyroStaticBiasZ'] * (math.pi / 180)
+    msg.count = data.get('Count', 0)
+    msg.timestamp = data.get('Timestamp', 0.0)
+    msg.pitch = data.get('Pitch', 0.0)
+    msg.roll = data.get('Roll', 0.0)
+    msg.yaw = data.get('Yaw', 0.0)
+    msg.imu_status = data.get('IMUStatus', 0)
+
+    def deg_to_rad_safe(val):
+        return (val or 0.0) * (math.pi / 180)
+
+    msg.gyro_bias_x = deg_to_rad_safe(data.get('GyroBiasX'))
+    msg.gyro_bias_y = deg_to_rad_safe(data.get('GyroBiasY'))
+    msg.gyro_bias_z = deg_to_rad_safe(data.get('GyroBiasZ'))
+    msg.gyro_static_bias_x = deg_to_rad_safe(data.get('GyroStaticBiasX'))
+    msg.gyro_static_bias_y = deg_to_rad_safe(data.get('GyroStaticBiasY'))
+    msg.gyro_static_bias_z = deg_to_rad_safe(data.get('GyroStaticBiasZ'))
+
     pub.publish(msg)
-    # rospy.loginfo(msg._type)
 
 def main():
     serial_port = initialize_serial_port()
@@ -116,7 +127,7 @@ def main():
         rospy.loginfo("SerialPort Open")
         while not rospy.is_shutdown():
             data = serial_com.get_data()
-            while len(data) < USED_FRAME_LEN:
+            while len(data) < MIN_FRAME_LEN:
                 data += serial_com.get_data()
             try:
                 parsed_data = parse_frame(data)
--- scripts/parser.py
+++ scripts/parser.py
@@ -11,8 +11,8 @@ CMD_LEN = 1
 LEN_LEN = 1
 CRC_LEN = 1
 TAIL_LEN = 2
-USED_FRAME_LEN = 68
-MIN_FRAME_LEN = 8
+FRAME_LEN_OLD = 68
+FRAME_LEN_BINARY = 72
 
 
 def checksum_crc(data, crc_ref):
@@ -40,44 +40,92 @@ def checksum_crc(data, crc_ref):
 # Parse frame data
 def parse_frame(frame):
 
-    if len(frame) == USED_FRAME_LEN:
+    if len(frame) == FRAME_LEN_OLD:
         head = frame[0:HEAD_LEN]
         dom = frame[HEAD_LEN:HEAD_LEN + DOM_LEN]
         cmd = frame[HEAD_LEN + DOM_LEN:HEAD_LEN + DOM_LEN + CMD_LEN]
         length = frame[HEAD_LEN + DOM_LEN + CMD_LEN:HEAD_LEN + DOM_LEN + CMD_LEN + LEN_LEN]
 
-        cmd_value = struct.unpack('B', cmd)[0]
+        # cmd_value = struct.unpack('B', cmd)[0]
         Data_Len = struct.unpack('B', length)[0]
 
         data = frame[HEAD_LEN + DOM_LEN + CMD_LEN + LEN_LEN:HEAD_LEN + DOM_LEN + CMD_LEN + LEN_LEN + Data_Len]
         crc = frame[HEAD_LEN + DOM_LEN + CMD_LEN + LEN_LEN + Data_Len:
                     HEAD_LEN + DOM_LEN + CMD_LEN + LEN_LEN + Data_Len + CRC_LEN]
 
-        if cmd_value == 1 and len(data) == Data_Len and checksum_crc(head + dom + cmd + length + data, crc):
+        if len(data) == Data_Len and checksum_crc(head + dom + cmd + length + data, crc):
             parsed_data = {
                 'Count': struct.unpack('B', data[0:1])[0],
-                'Timestamp': struct.unpack('<Q', data[1:9])[0],  # Corrected slice for 8 bytes
-                'AccX': struct.unpack('<f', data[9:13])[0],  # Corrected slice for 4 bytes
-                'AccY': struct.unpack('<f', data[13:17])[0],  # Corrected slice for 4 bytes
-                'AccZ': struct.unpack('<f', data[17:21])[0],  # Corrected slice for 4 bytes
-                'GyroX': struct.unpack('<f', data[21:25])[0],  # Corrected slice for 4 bytes
-                'GyroY': struct.unpack('<f', data[25:29])[0],  # Corrected slice for 4 bytes
-                'GyroZ': struct.unpack('<f', data[29:33])[0],  # Corrected slice for 4 bytes
-                'Pitch': struct.unpack('<f', data[33:37])[0],  # Corrected slice for 4 bytes
-                'Roll': struct.unpack('<f', data[37:41])[0],  # Corrected slice for 4 bytes
+                'Timestamp': struct.unpack('<Q', data[1:9])[0],
+                'AccX': struct.unpack('<f', data[9:13])[0],
+                'AccY': struct.unpack('<f', data[13:17])[0],
+                'AccZ': struct.unpack('<f', data[17:21])[0],
+                'GyroX': struct.unpack('<f', data[21:25])[0],
+                'GyroY': struct.unpack('<f', data[25:29])[0],
+                'GyroZ': struct.unpack('<f', data[29:33])[0],
+                'Pitch': struct.unpack('<f', data[33:37])[0],
+                'Roll': struct.unpack('<f', data[37:41])[0],
                 'Yaw': struct.unpack('<f', data[41:45])[0],
-                'Temperature': struct.unpack('<h', data[45:47])[0] * 0.1,  # Corrected slice for 2 bytes
+                'Quat0': None,
+                'Quat1': None,
+                'Quat2': None,
+                'Quat3': None,
+                'Temperature': struct.unpack('<h', data[45:47])[0] * 0.1,
                 'IMUStatus': struct.unpack('B', data[47:48])[0],
-                'GyroBiasX': struct.unpack('<h', data[48:50])[0] * 0.0001,  # Corrected slice for 2 bytes
-                'GyroBiasY': struct.unpack('<h', data[50:52])[0] * 0.0001,  # Corrected slice for 2 bytes
-                'GyroBiasZ': struct.unpack('<h', data[52:54])[0] * 0.0001,  # Corrected slice for 2 bytes
-                'GyroStaticBiasX': struct.unpack('<h', data[54:56])[0] * 0.0001,  # Corrected slice for 2 bytes
-                'GyroStaticBiasY': struct.unpack('<h', data[56:58])[0] * 0.0001,  # Corrected slice for 2 bytes
-                'GyroStaticBiasZ': struct.unpack('<h', data[58:60])[0] * 0.0001  # Corrected slice for 2 bytes
+                'GyroBiasX': struct.unpack('<h', data[48:50])[0] * 0.0001,
+                'GyroBiasY': struct.unpack('<h', data[50:52])[0] * 0.0001,
+                'GyroBiasZ': struct.unpack('<h', data[52:54])[0] * 0.0001,
+                'GyroStaticBiasX': struct.unpack('<h', data[54:56])[0] * 0.0001,
+                'GyroStaticBiasY': struct.unpack('<h', data[56:58])[0] * 0.0001,
+                'GyroStaticBiasZ': struct.unpack('<h', data[58:60])[0] * 0.0001
             }
             return parsed_data
         else:
             return None
+        
+    elif len(frame) == FRAME_LEN_BINARY:
+        head = frame[0:HEAD_LEN]
+        dom = frame[HEAD_LEN:HEAD_LEN + DOM_LEN]
+        cmd = frame[HEAD_LEN + DOM_LEN:HEAD_LEN + DOM_LEN + CMD_LEN]
+        length = frame[HEAD_LEN + DOM_LEN + CMD_LEN:HEAD_LEN + DOM_LEN + CMD_LEN + LEN_LEN]
+
+        # cmd_value = struct.unpack('B', cmd)[0]
+        Data_Len = struct.unpack('B', length)[0]
+
+        data = frame[HEAD_LEN + DOM_LEN + CMD_LEN + LEN_LEN:HEAD_LEN + DOM_LEN + CMD_LEN + LEN_LEN + Data_Len]
+        crc = frame[HEAD_LEN + DOM_LEN + CMD_LEN + LEN_LEN + Data_Len:
+                    HEAD_LEN + DOM_LEN + CMD_LEN + LEN_LEN + Data_Len + CRC_LEN]
+
+        if len(data) == Data_Len and checksum_crc(head + dom + cmd + length + data, crc):
+            parsed_data = {
+                'Count': struct.unpack('B', data[0:1])[0],
+                'Timestamp': struct.unpack('<Q', data[1:9])[0],
+                'AccX': struct.unpack('<f', data[9:13])[0],
+                'AccY': struct.unpack('<f', data[13:17])[0],
+                'AccZ': struct.unpack('<f', data[17:21])[0],
+                'GyroX': struct.unpack('<f', data[21:25])[0],
+                'GyroY': struct.unpack('<f', data[25:29])[0],
+                'GyroZ': struct.unpack('<f', data[29:33])[0],
+                'Pitch': struct.unpack('<f', data[33:37])[0],
+                'Roll': struct.unpack('<f', data[37:41])[0],
+                'Yaw': struct.unpack('<f', data[41:45])[0],
+                'Quat0': struct.unpack('<f', data[45:49])[0],
+                'Quat1': struct.unpack('<f', data[49:53])[0],
+                'Quat2': struct.unpack('<f', data[53:57])[0],
+                'Quat3': struct.unpack('<f', data[57:61])[0],
+                'Temperature': struct.unpack('<h', data[61:63])[0] * 0.1,
+                'IMUStatus': struct.unpack('B', data[63:64])[0],
+                'GyroBiasX': None,
+                'GyroBiasY': None,
+                'GyroBiasZ': None,
+                'GyroStaticBiasX': None,
+                'GyroStaticBiasY': None,
+                'GyroStaticBiasZ': None
+            }
+            return parsed_data
+        else:
+            return None
+        
     else:
         return None
 
